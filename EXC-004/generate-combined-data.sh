8808a9d404876c9888d1b1d6b2f2ab90847bfb14
Al Sahli Hussein <husseinsahly.hs@gmail.com>
2025-02-13 21:47:07 +0100

    generate-combined-data.sh

---
+++ EXC-004/generate-combined-data.sh
@@ -0,0 +1,82 @@
+mkdir COMBINED-DATA
+
+for dir in ./RAW-DATA/*/
+do 
+	MAG_COUNT=1
+    BIN_COUNT=1
+
+	for file in "$dir"bins/*.fasta
+	do 
+		XXX=$(grep "$(basename "$dir" /)" './RAW-DATA/sample-translation.txt' | awk '{print $2}')
+		
+	if [[ "$file" != "${dir}bins/bin-unbinned.fasta" ]] 
+	then
+	        
+		comp=$(grep "$(basename "$file" .fasta)" "${dir}/checkm.txt" |  awk '{print $13}')
+	        cont=$(grep "$(basename "$file" .fasta)" "${dir}/checkm.txt" |  awk '{print $14}')
+		
+		if (( $(echo "$comp >= 50.00" | bc -l) )) && (( $(echo "$cont <= 5.00" | bc -l) ))
+		then
+			YYY="MAG"
+                ZZZ=$(printf "%03d" $MAG_COUNT) 
+                ((MAG_COUNT++))  
+            else
+                YYY="BIN"
+                ZZZ=$(printf "%03d" $BIN_COUNT)
+                ((BIN_COUNT++))  
+
+		fi
+
+		echo "${XXX}_${YYY}_${ZZZ}.fa"
+		cp "$file" "./COMBINED-DATA/${XXX}_${YYY}_${ZZZ}.fa"
+		
+
+	fi
+
+                if [[ "$file" == "${dir}bins/bin-unbinned.fasta" ]]
+		then
+                echo "${XXX}_UNBINNED.fa"
+
+	        cp "$file" "./COMBINED-DATA/${XXX}_UNBINNED.fa"
+                	
+	fi
+
+
+	done
+  
+for checkm in "$dir"checkm.txt
+do
+                XXX=$(grep "$(basename "$dir" /)" './RAW-DATA/sample-translation.txt' | awk '{print $2}')
+
+		        echo "$XXX-CHECKM.txt"
+
+			cp "$checkm" "./COMBINED-DATA/${XXX}-CHECKM.txt"
+	done
+
+for gtdb in "$dir"gtdb.gtdbtk.tax
+do
+                XXX=$(grep "$(basename "$dir" /)" './RAW-DATA/sample-translation.txt' | awk '{print $2}')
+
+		        echo "$XXX-GTDB-TAX.txt"
+
+			cp "$gtdb" "./COMBINED-DATA/${XXX}-GTDB-TAX.txt"	
+	done
+		
+
+
+
+
+
+
+
+
+
+
+done
+
+for fa in ./COMBINED-DATA/*.fa
+do
+	SSS=$(basename "$fa" .fa | awk -F_ '{print $1}')
+    awk -v var="$SSS" '{if ($0 ~ /^>/) { sub(/^>[^:]+:/, ">" var ":"); print } else print }' "$fa" > "$fa.tmp" && mv "$fa.tmp" "$fa"
+
+done
