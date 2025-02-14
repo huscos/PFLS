54911c7c99520aa52b73dc7a7a341eaddad76db2
Al Sahli Hussein <husseinsahly.hs@gmail.com>
2025-02-14 10:02:41 +0100

    generate-combined-data.sh

--- EXC-004/generate-combined-data.sh
+++ EXC-004/generate-combined-data.sh
@@ -1,23 +1,28 @@
-mkdir COMBINED-DATA
+if [  -d ./COMBINED-DATA ]; then
+        rm -rf COMBINED-DATA
+        mkdir COMBINED-DATA
+
+    else mkdir COMBINED-DATA
+fi
 
 for dir in ./RAW-DATA/*/
 do 
-	MAG_COUNT=1
+        MAG_COUNT=1
     BIN_COUNT=1
 
-	for file in "$dir"bins/*.fasta
-	do 
-		XXX=$(grep "$(basename "$dir" /)" './RAW-DATA/sample-translation.txt' | awk '{print $2}')
-		
-	if [[ "$file" != "${dir}bins/bin-unbinned.fasta" ]] 
-	then
-	        
-		comp=$(grep "$(basename "$file" .fasta)" "${dir}/checkm.txt" |  awk '{print $13}')
-	        cont=$(grep "$(basename "$file" .fasta)" "${dir}/checkm.txt" |  awk '{print $14}')
-		
-		if (( $(echo "$comp >= 50.00" | bc -l) )) && (( $(echo "$cont <= 5.00" | bc -l) ))
-		then
-			YYY="MAG"
+        for file in "$dir"bins/*.fasta
+        do 
+                XXX=$(grep "$(basename "$dir" /)" './RAW-DATA/sample-translation.txt' | awk '{print $2}')
+
+        if [[ "$file" != "${dir}bins/bin-unbinned.fasta" ]] 
+        then
+                
+                comp=$(grep "$(basename "$file" .fasta)" "${dir}/checkm.txt" |  awk '{print $13}')
+                cont=$(grep "$(basename "$file" .fasta)" "${dir}/checkm.txt" |  awk '{print $14}')
+
+                if (( $(echo "$comp >= 50.00" | bc -l) )) && (( $(echo "$cont <= 5.00" | bc -l) ))
+                then
+                        YYY="MAG"
                 ZZZ=$(printf "%03d" $MAG_COUNT) 
                 ((MAG_COUNT++))  
             else
@@ -25,43 +30,43 @@ do
                 ZZZ=$(printf "%03d" $BIN_COUNT)
                 ((BIN_COUNT++))  
 
-		fi
+                fi
 
-		echo "${XXX}_${YYY}_${ZZZ}.fa"
-		cp "$file" "./COMBINED-DATA/${XXX}_${YYY}_${ZZZ}.fa"
-		
+                echo "${XXX}_${YYY}_${ZZZ}.fa"
+                cp "$file" "./COMBINED-DATA/${XXX}_${YYY}_${ZZZ}.fa"
 
-	fi
+
+        fi
 
                 if [[ "$file" == "${dir}bins/bin-unbinned.fasta" ]]
-		then
+                then
                 echo "${XXX}_UNBINNED.fa"
 
-	        cp "$file" "./COMBINED-DATA/${XXX}_UNBINNED.fa"
-                	
-	fi
+                cp "$file" "./COMBINED-DATA/${XXX}_UNBINNED.fa"
+                
+        fi
 
 
-	done
+        done
   
 for checkm in "$dir"checkm.txt
 do
                 XXX=$(grep "$(basename "$dir" /)" './RAW-DATA/sample-translation.txt' | awk '{print $2}')
 
-		        echo "$XXX-CHECKM.txt"
+                        echo "$XXX-CHECKM.txt"
 
-			cp "$checkm" "./COMBINED-DATA/${XXX}-CHECKM.txt"
-	done
+                        cp "$checkm" "./COMBINED-DATA/${XXX}-CHECKM.txt"
+        done
 
 for gtdb in "$dir"gtdb.gtdbtk.tax
 do
                 XXX=$(grep "$(basename "$dir" /)" './RAW-DATA/sample-translation.txt' | awk '{print $2}')
 
-		        echo "$XXX-GTDB-TAX.txt"
+                        echo "$XXX-GTDB-TAX.txt"
 
-			cp "$gtdb" "./COMBINED-DATA/${XXX}-GTDB-TAX.txt"	
-	done
-		
+                        cp "$gtdb" "./COMBINED-DATA/${XXX}-GTDB-TAX.txt"
+        done
+
 
 
 
@@ -76,7 +81,7 @@ done
 
 for fa in ./COMBINED-DATA/*.fa
 do
-	SSS=$(basename "$fa" .fa | awk -F_ '{print $1}')
-    awk -v var="$SSS" '{if ($0 ~ /^>/) { sub(/^>[^:]+:/, ">" var ":"); print } else print }' "$fa" > "$fa.tmp" && mv "$fa.tmp" "$fa"
+        FILENAME=$(basename "$fa" .fa)  # Extract filename without .fa
+    awk -v prefix="$FILENAME" '{if ($0 ~ /^>/) print ">" prefix "_" sprintf("%05d", ++i); else print}' "$fa" > "$fa.tmp" && mv "$fa.tmp" "$fa"
 
 done
